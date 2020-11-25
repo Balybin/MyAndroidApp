4bc6e47e882feb37f5358d39910afa3b09baccd7
balybin <balybin.iwan@yandex.ru>
2020-11-25 17:20:52 +0700

    коммит, чтобы гит дал сделать pull request

--- app/src/main/java/com/rdsht/myandroidapp/MainActivity.kt
+++ app/src/main/java/com/rdsht/myandroidapp/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+        // changes
+        // test comment to commit
     }
 }
