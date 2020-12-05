2a16cf672515d9130b1b609757d7f792524ee123
balybin <balybin.iwan@yandex.ru>
2020-12-06 03:16:19 +0700

    homework 3

--- app/src/main/java/com/rdsht/myandroidapp/MainActivity.kt
+++ app/src/main/java/com/rdsht/myandroidapp/MainActivity.kt
@@ -2,13 +2,41 @@ package com.rdsht.myandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.FrameLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentMoviesList.ClickListener,
+    FragmentMovieDetails.MovieDetailsOnClickListener {
+
+    private val listFragment = FragmentMoviesList().apply { setListener(this@MainActivity) }
+    private val detailfragment = FragmentMovieDetails().apply { setListener(this@MainActivity) };
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
-        // changes
-        // test comment to commit
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.anim.enter_from_right, R.anim.exit_to_left,
+                R.anim.enter_from_left, R.anim.exit_to_right
+            )
+            .add(R.id.fragment_container, listFragment)
+            .commit()
     }
+
+    override fun gotoDetail() {
+        supportFragmentManager.beginTransaction().setCustomAnimations(
+            R.anim.enter_from_right, R.anim.exit_to_left,
+            R.anim.enter_from_left, R.anim.exit_to_right
+        ).remove(listFragment)
+            .add(R.id.fragment_container, detailfragment).commit()
+    }
+
+    override fun back() {
+        supportFragmentManager.beginTransaction().setCustomAnimations(
+            R.anim.enter_from_right, R.anim.exit_to_left,
+            R.anim.enter_from_left, R.anim.exit_to_right
+        ).remove(detailfragment)
+            .add(R.id.fragment_container, listFragment).commit()
+    }
+
 }
